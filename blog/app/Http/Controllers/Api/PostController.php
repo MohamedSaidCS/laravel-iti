6d0175281a6362d13be1abf4738e9edf48b5449d
Mohamed <mohamed.said.10398@gmail.com>
2022-04-24 07:09:51 +0200

    API endpoints, Sanctum, Eager Loading

---
+++ blog/app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use App\Http\Resources\PostResource;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::all();
+
+        return PostResource::collection($posts);
+    }
+
+    public function show($postId)
+    {
+        $post = Post::find($postId);
+
+        if(!$post) {
+            return response()->json([
+                'message' => 'Post not found'
+            ], 404);
+        }
+
+        return new PostResource($post);
+    }
+
+    public function store()
+    {
+        request()->validate([
+            'title' => 'required|min:3|unique:posts',
+            'description' => 'required|min:10',
+            'user_id' => 'required|exists:users,id',
+        ]);
+
+        $data = request()->all();
+
+          $post = Post::create([
+            'title' => $data['title'],
+            'description' => $data['description'],
+            'user_id' => $data['user_id'],
+
+        ]);
+
+        return new PostResource($post);
+    }
+}
--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -18,6 +18,8 @@ class Post extends Model
         'image'
     ];
 
+    protected $with = ['user'];
+
     public function user() {
         return $this->belongsTo(User::class);
     }
